a3848e7905a08d1e5da6934281e06594c101ebec
shaniece crumpler <s.crumpler11@gmail.com>
2019-07-12 22:53:24 -0400

    atyling packages compenent

--- src/components/Packages/Packages.js
+++ src/components/Packages/Packages.js
@@ -15,41 +15,48 @@ const Packages =()=>{
           <tr>
             <th scope="row">1 Lesson <br></br>
             $40</th>
-            <td>Mark</td>
-            <td>Otto</td>
+            <td>$130</td>
+            <td>$20</td>
           </tr>
           <tr>
           <th scope="row">6 Lessons <br></br>
             $224</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
+            <td>$320</td>
+            <td>Free</td>
           </tr>
           <tr>
           <th scope="row">8 Lessons <br></br>
             $305</th>
-            <td>Larry</td>
-            <td>the Bird</td>
+            <td>$405</td>
+            <td>Free</td>
           </tr>
           <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
+          <th scope="row">10 Lessons <br></br>
+            $380</th>
+             <td>$480</td>
+            <td>Free</td>
           </tr>
           <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
+          <th scope="row">12 Lessons <br></br>
+            $470</th>
+            <td>$565</td>
+            <td>Free </td>
           </tr>
           <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-          </tr>
+              
+              <th scope="row">15 Lessons <br></br>
+                $580</th>
+                 <td>$670</td>
+                <td>Free</td>
+              </tr>
           <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
+              
+          <th scope="row">20 Lessons <br></br>
+            $740</th>
+             <td>$850</td>
+            <td>Free</td>
           </tr>
+         
         </tbody>
       </table>
     )
